3630632d10db6dda7bc8216624bfabc1e15fad02
Matic Jurglic <matic@jurglic.si>
2016-05-14 21:44:21 +0200

    prevent The ember generate <entity-name> command requires an entity name to be specified

--- blueprints/ember-chartist-shim/index.js
+++ blueprints/ember-chartist-shim/index.js
@@ -1,5 +1,10 @@
 /*jshint node:true*/
 module.exports = {
+  normalizeEntityName: function() {
+    // allows to run ember -g ember-chartist-shim and not blow up
+    // because ember cli normally expects the format
+    // ember generate <entitiyName> <blueprint>
+  },
   afterInstall: function(options) {
     return this.addBowerPackageToProject('chartist');
   }
