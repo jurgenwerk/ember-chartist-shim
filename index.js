daaa9fd7f84ed6c4b6826fa95b9a260714bfbf5a
Matic Jurglic <matic@jurglic.si>
2016-05-14 15:52:54 +0200

    add Chartist shim as a module

--- index.js
+++ index.js
@@ -2,5 +2,15 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-chartist-shim'
+  name: 'chartist',
+  included: function included(app) {
+    this._super.included(app);
+    app.import(app.bowerDirectory + '/chartist/dist/chartist.css');
+    app.import(app.bowerDirectory + '/chartist/dist/chartist.js');
+    app.import('vendor/chartist.js', {
+      exports: {
+        Chartist: ['default']
+      }
+    });
+  }
 };
